594fa359c33342f2c178a3e28a63467b2f03e6f4
DYZparker <qq182345999@163.com>
2021-09-11 22:47:21 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import {Button, Col} from 'antd';
+import { BrowserRouter } from 'react-router-dom';
+import { Globalstyle, WrapperRow, WrapperBackTop } from './style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="App">
+    <Globalstyle />
+    <BrowserRouter>
+      {/* <Store> */}
+        {/* <Header /> */}
+        <WrapperRow justify="center">
+          <Col className="main-left" xs={24} sm={24} md={24} lg={17} xl={11}>
+            {/* <RouterList /> */}
+            <div className="App">
+              <Button type="primary">Button</Button>
+            </div>
+          </Col>
+          <Col className="main-right" xs={0} sm={0} md={0} lg={6} xl={4}>
+            {/* <Sider /> */}
+          </Col>
+        </WrapperRow>
+        {/* <Footer /> */}
+      {/* </Store> */}
+    </BrowserRouter>
+    <WrapperBackTop>
+      {/* <ToTopOutlined /> */}
+    </WrapperBackTop>
+  </div>
+)
 
 export default App;
